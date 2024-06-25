da7b446159846f4745a18d276ce23cb815340397
D <166647954+mmjustme@users.noreply.github.com>
2024-06-25 13:50:16 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./global.scss";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="name_section">
+        <div>
+          <h1>Dmytro Kurdelchuk</h1>
+          <h3>Fullstack Web Developer</h3>
+          <div className="contacts_section">
+            <p>dmytro.kurdelchuk.@gmail.com</p>
+            <p>+380988702388</p>
+            <p>Ukraine, Lutsk</p>
+          </div>
+        </div>
+        <img src="" alt="profile-photo" />
+      </div>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
